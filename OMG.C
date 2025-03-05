409d9706324058d9c30881218f3b3f80e9af9602
HimaMyth <viraltiktok331@gmail.com>
2025-03-05 17:17:25 +0300

    OMG.C
    Alot of OMG printing, open the OMG.exe and look at it, if you wanna config it, config it in OMG.C

---
+++ OMG.C
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+    while (true)
+    {
+        printf("OMG");
+        printf("O M G");
+        printf("O  M  G");
+        printf("O M G");
+        printf("OMG");
+    }
+    return 0;
+}
